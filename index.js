b9b5a2db950dbf1152bd807176ea9d1c0df20a46
YuryKharytanovich <yurykharytanovich@gmail.com>
2017-10-18 21:28:10 +0300

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello fuck suck bitch World!')
+  res.send('Hello fuck suck it bitch World!')
 })
 
 app.listen(process.env.PORT || 3000, function () {
