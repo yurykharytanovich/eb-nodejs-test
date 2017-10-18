429063445a023d879a2b640ca9e1e2c5f1b791a7
YuryKharytanovich <yurykharytanovich@gmail.com>
2017-10-18 21:04:19 +0300

    fix port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello fuck suck bitch World!')
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
